13e0122366ff2b123ce1a091888dbfccda1703c4
mushanyu <2910714313@qq.com>
2025-06-09 00:07:09 +0800

    qreflect and string

---
+++ pkg/util/string.go
@@ -0,0 +1,45 @@
+package util
+
+import "strings"
+
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+func isDigit(b byte) bool {
+	return '0' <= b && b <= '9'
+}
+
+func toLower(b byte) byte {
+	if isUpper(b) {
+		return b - 'A' + 'a'
+	}
+	return b
+}
+
+// UnderscoreName 将驼峰命名转换为下划线命名
+func UnderscoreName(name string) string {
+	var buf strings.Builder
+	buf.Grow(len(name) * 2)
+
+	for i := 0; i < len(name); i++ {
+		buf.WriteByte(toLower(name[i]))
+		if i != len(name)-1 && isUpper(name[i+1]) &&
+			(isLower(name[i]) || isDigit(name[i]) ||
+				(i != len(name)-2 && isLower(name[i+2]))) {
+			buf.WriteByte('_')
+		}
+	}
+	return buf.String()
+}
+
+// CamelName 将下划线命名转换为驼峰命名
+func CamelName(name string) string {
+	name = strings.Replace(name, "_", " ", -1)
+	name = strings.Title(name)
+	return strings.Replace(name, " ", "", -1)
+}
